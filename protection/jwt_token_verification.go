d744ce07d752abbbefbb18f2f0f3591bfa935d15
Prothes Barai <aspprothesbarai1619@gmail.com>
2026-03-29 17:54:12 +0600

    tkn

--- protection/jwt_token_verification.go
+++ protection/jwt_token_verification.go
@@ -4,23 +4,41 @@ import (
 	"go-post-api-projects/logger"
 	"os"
 	"time"
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
 )
 
+// >>>  Get Secret Key From .env File
 var secretKey = []byte(getSecret())
-func getSecret() string{
+
+func getSecret() string {
 	secret := os.Getenv("JWT_SECRET")
-	if(secret == ""){logger.AppLogger.Error.Println("Not Found SECRET KEY in env file")}
+	if(secret == ""){
+		logger.AppLogger.Error.Println("JWT_SECRET not set in environment")
+	}
 	return secret
 }
 
 
-func GeneratedToken(username string) (string,error){
-	presentTime := time.Now()
-	
-	expireTime := time.Date(presentTime.Year(),presentTime.Month(),presentTime.Day(),12,0,0,0,presentTime.Location())
+/// >> Generated Token & Every Day 12:00PM expire its
+func GenerateToken(username string) (string, error) {
+    // 👉 BD timezone set করা
+    loc, _ := time.LoadLocation("Asia/Dhaka")
 
-	if presentTime.After(expireTime){expireTime = expireTime.Add(24 * time.Hour)}
+    presentTime := time.Now().In(loc)
 
+    //  আজকের রাত 12:00 (midnight)
+    expireTime := time.Date(presentTime.Year(),presentTime.Month(),presentTime.Day(), 0, 0, 0, 0,loc,)
 
+    // যদি এখন সময় already রাত 12টা পার হয়ে যায়
+    if !presentTime.Before(expireTime) {
+        expireTime = expireTime.Add(24 * time.Hour)
+    }
+
+    claims := jwt.MapClaims{
+        "username": username,
+        "exp":      expireTime.Unix(),
+    }
+
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+    return token.SignedString(secretKey)
 }
